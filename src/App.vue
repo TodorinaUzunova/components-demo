0a87bd7532b5a98e83979c797e25f0423e8ab618
TodorinaUzunova <toschkam@abv.bg>
2020-03-15 15:22:37 +0200

    initial commit file.txt

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,46 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductList :products="products" :title="title" @onChangeTitle="title=$event"/>
+  <!-- <Counter :counter="counter"/>
+  <Counter :counter="counter"/>
+  <Counter :counter="counter"/> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+//import Counter from './components/Counter.vue'
+import ProductList from './components/ProductList.vue'
 
 export default {
   name: 'App',
+  data:function(){
+    return {
+      counter:0,
+      products:[
+        {
+          type:'Spaghetti', price:11, ingredients:['Pasta', 'Meatballs', 'Sauce']
+        },
+        {
+          type:'Musaka', price:12, ingredients:['Potatoes', 'Mince', 'Onion']
+          },
+        {
+          type:'Cake', price:14, ingredients:['Flouer', 'Oil', 'Sugar']
+          },
+      ],
+      title:'This is my product list!',
+    }},
+    watch:{
+      title(newValue, oldValue){
+      console.log([newValue, oldValue])
+
+      }
+    },
   components: {
-    HelloWorld
+    HelloWorld,
+     //Counter,
+     ProductList
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import Counter from './components/Counter.vue';
 
+//Vue.component('Counter', Counter);  //globalna registracia
 Vue.config.productionTip = false
 
 new Vue({
