a6436d8606be48f97c2ee2830de8e12520a4753b
TodorinaUzunova <toschkam@abv.bg>
2020-03-15 15:40:52 +0200

    new file.txt

---
+++ src/components/Product.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+      <!-- <input @input="changeType"/> -->
+    <h1>Type: {{mealType}}</h1>
+    <h1>Price:{{price}}</h1>
+     <ul>
+        <li v-for="(ing, i) in ingredients" :key="i">{{ing}}</li>
+     </ul>
+  <div>{{expireDate}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props:{
+      mealType:{type:String,
+       required:true},
+      price:Number,
+      ingredients:{
+          type:Array,
+          validator:(arr)=>{
+              return arr.every(i=> typeof i==="string")&& arr.every((i)=>i[0].toUpperCase(i[0]));
+          }
+          },
+      expireDate:{
+          type:Date,
+          default: ()=> new Date(),
+      }
+    },
+    methods:{
+//   changeType(e){
+//      const value=e.target.value;  !!! ne se prawi, props are immutable
+//      this.type=value;
+//   }
+        }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{title}}</h1>
+    <input :value="title" @input="changeTitle" />
+    <Product
+      v-for="(p,i) in products"
+      :meal-type="p.type"
+      :price="p.price"
+      :ingredients="p.ingredients"
+      :key="i"
+    />
+  </div>
+</template>
+
+
+<script>
+import Product from "./Product.vue";
+export default {
+  name: "ProductList",
+  props: {
+       title: String,
+    products: Array,
+   
+  },
+  data: function() {
+    return {}
+  },
+  components: {
+    Product
+  },
+  methods: {
+    changeTitle(e) {
+      const value = e.target.value;
+      this.$emit("onChangeTitle", value);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
